7b7daf82c796e92982631b55f35d4f69717ee875
br7forlife <diazpachecobrayan@gmail.com>
2025-09-10 10:54:30 -0500

    started project DistributorStore

---
+++ DistributorStore/src/main/java/com/archives/DistributorStore/DistributorStoreApplication.java
@@ -0,0 +1,13 @@
+package com.archives.DistributorStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributorStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributorStoreApplication.class, args);
+	}
+
+}
---
+++ DistributorStore/src/test/java/com/archives/DistributorStore/DistributorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archives.DistributorStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributorStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
