85238e941482288be002a8caa9524d959756a7ed
br7forlife <diazpachecobrayan@gmail.com>
2025-09-19 12:58:44 -0500

    create to Dto for not use StoreModel

--- DistributorStore/src/main/java/com/archives/DistributorStore/security/CustomUserDetails.java
+++ DistributorStore/src/main/java/com/archives/DistributorStore/security/CustomUserDetails.java
@@ -1,27 +1,30 @@
 package com.archives.DistributorStore.security;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public record CustomUserDetails(
-    
+        String name,
+        String password,
+        List<GrantedAuthority> authorities
 
 ) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return name;
     }
 }
