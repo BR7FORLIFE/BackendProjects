a20e5e283320cfe238e5125967c1e763a753df80
br7forlife <diazpachecobrayan@gmail.com>
2025-09-28 17:04:29 -0500

    create some rols for users

--- DistributorStore/src/main/java/com/archives/DistributorStore/enums/Rols.java
+++ DistributorStore/src/main/java/com/archives/DistributorStore/enums/Rols.java
@@ -1,5 +1,9 @@
 package com.archives.DistributorStore.enums;
 
 public enum Rols {
-    DISTRIBUTOR, DELIVERY, PRESALE, STORE
+    DISTRIBUTOR, DELIVERY, PRESALE, STORE;
+
+    public String asAuthority() {
+        return "ROLE_" + name();
+    }
 }
