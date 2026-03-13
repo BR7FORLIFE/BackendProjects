87fce7d0ee88ddfdd8f6d4ff9b6f1790b74b23c9
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-03-13 15:01:31 -0500

    feat: started in the MessagingUseCase implementation for send records in kafka

--- IA-Websockets/src/main/java/com/archives/IAWebsockets/application/messaging/orchestators/MessagingUseCaseImp.java
+++ IA-Websockets/src/main/java/com/archives/IAWebsockets/application/messaging/orchestators/MessagingUseCaseImp.java
@@ -1,12 +1,26 @@
 package com.archives.IAWebsockets.application.messaging.orchestators;
 
+import org.springframework.stereotype.Service;
+
+import com.archives.IAWebsockets.application.messaging.ports.MessagePublisherPort;
 import com.archives.IAWebsockets.application.messaging.usecases.MessagingUseCase;
+import com.archives.IAWebsockets.domain.messaging.ChatMessageRequestEvent;
 
 import reactor.core.publisher.Mono;
 
+@Service
 public class MessagingUseCaseImp implements MessagingUseCase {
+
+    // puerto para comunicarse con el servicio de kafka
+    private final MessagePublisherPort messagePublisher;
+
+    public MessagingUseCaseImp(MessagePublisherPort messagePublisher) {
+        this.messagePublisher = messagePublisher;
+    }
+
     @Override
-    public Mono<Void> handleMessage(String message) {
-        return null;
+    public Mono<Void> handleMessage(String sessionId, String message) {
+        ChatMessageRequestEvent messageEvent = new ChatMessageRequestEvent(sessionId, message);
+        return messagePublisher.publishChatRequest(messageEvent);
     }
 }
