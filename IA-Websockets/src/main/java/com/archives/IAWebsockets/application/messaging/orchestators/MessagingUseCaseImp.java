b5578ca7a0184279f749f75dd507c13a851d675c
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-03-14 17:43:57 -0500

    feat: create messaging use case and use case implementation for the kafka flow for example send record in kafka and recive in another flow in IA use case

--- IA-Websockets/src/main/java/com/archives/IAWebsockets/application/messaging/orchestators/MessagingUseCaseImp.java
+++ IA-Websockets/src/main/java/com/archives/IAWebsockets/application/messaging/orchestators/MessagingUseCaseImp.java
@@ -2,25 +2,38 @@ package com.archives.IAWebsockets.application.messaging.orchestators;
 
 import org.springframework.stereotype.Service;
 
+import com.archives.IAWebsockets.application.messaging.ports.MessageCustomerPort;
 import com.archives.IAWebsockets.application.messaging.ports.MessagePublisherPort;
 import com.archives.IAWebsockets.application.messaging.usecases.MessagingUseCase;
 import com.archives.IAWebsockets.domain.messaging.ChatMessageRequestEvent;
+import com.archives.IAWebsockets.domain.messaging.ChatMessageResponseEvent;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class MessagingUseCaseImp implements MessagingUseCase {
 
-    // puerto para comunicarse con el servicio de kafka
+    // puerto para comunicarse con el servicio de kafka como productor (producer)
     private final MessagePublisherPort messagePublisher;
 
-    public MessagingUseCaseImp(MessagePublisherPort messagePublisher) {
+    // puerto para comunicarse con el servicio de kafka como consumidor (consumer)
+    private final MessageCustomerPort messageCustomer;
+
+    public MessagingUseCaseImp(MessagePublisherPort messagePublisher, MessageCustomerPort messageCustomer) {
         this.messagePublisher = messagePublisher;
+        this.messageCustomer = messageCustomer;
     }
 
     @Override
     public Mono<Void> handleMessage(String sessionId, String message) {
         ChatMessageRequestEvent messageEvent = new ChatMessageRequestEvent(sessionId, message);
         return messagePublisher.publishChatRequest(messageEvent);
+    }
+
+    @Override
+    public Flux<ChatMessageResponseEvent> handleResponse(String sessionId) {
+        return messageCustomer.receiveChatResponse()
+                .filter(event -> event.getSessionId().equals(sessionId));
     }
 }
--- IA-Websockets/src/main/java/com/archives/IAWebsockets/application/messaging/usecases/MessagingUseCase.java
+++ IA-Websockets/src/main/java/com/archives/IAWebsockets/application/messaging/usecases/MessagingUseCase.java
@@ -1,7 +1,12 @@
 package com.archives.IAWebsockets.application.messaging.usecases;
 
+import com.archives.IAWebsockets.domain.messaging.ChatMessageResponseEvent;
+
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface MessagingUseCase {
     Mono<Void> handleMessage(String sessionId, String message);
+
+    Flux<ChatMessageResponseEvent> handleResponse(String sessionId);
 }
