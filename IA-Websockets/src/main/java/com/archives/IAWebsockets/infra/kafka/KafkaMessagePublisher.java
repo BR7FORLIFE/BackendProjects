b06c31db4e9a731bb3e581f8e29a6bf5cbb33e7c
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-03-14 17:47:25 -0500

    feat: implementation of Producer in Apache Kafka

--- IA-Websockets/src/main/java/com/archives/IAWebsockets/infra/kafka/KafkaMessagePublisher.java
+++ IA-Websockets/src/main/java/com/archives/IAWebsockets/infra/kafka/KafkaMessagePublisher.java
@@ -1,17 +1,37 @@
 package com.archives.IAWebsockets.infra.kafka;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Component;
 
 import com.archives.IAWebsockets.application.messaging.ports.MessagePublisherPort;
 import com.archives.IAWebsockets.domain.messaging.ChatMessageRequestEvent;
 
 import reactor.core.publisher.Mono;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderRecord;
 
+//productor de kafka
 @Component
 public class KafkaMessagePublisher implements MessagePublisherPort {
 
+    private final KafkaSender<String, ChatMessageRequestEvent> sender; // llamamos al sender de kafka
+
+    public KafkaMessagePublisher(KafkaSender<String, ChatMessageRequestEvent> sender) {
+        this.sender = sender;
+    }
+
     @Override
     public Mono<Void> publishChatRequest(ChatMessageRequestEvent event) {
-        return null;
+        /**
+         * SenderRecord -> record para enviar el evento a kafka
+         */
+        SenderRecord<String, ChatMessageRequestEvent, String> record = SenderRecord.create(
+                new ProducerRecord<>(
+                        "chat-requests",
+                        event.getSessionId(),
+                        event),
+                event.getSessionId());
+
+        return sender.send(Mono.just(record)).then();
     }
 }
