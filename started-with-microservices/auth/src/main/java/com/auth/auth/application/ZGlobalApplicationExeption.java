7ffc7e8f89f8649da641681d86c027c7ad64c15f
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-05-21 13:38:14 -0500

    feat: add ZGlobalApplicationException class for custom exception handling

---
+++ started-with-microservices/auth/src/main/java/com/auth/auth/application/ZGlobalApplicationExeption.java
@@ -0,0 +1,8 @@
+package com.auth.auth.application;
+
+public class ZGlobalApplicationExeption extends RuntimeException {
+
+    public ZGlobalApplicationExeption(String message) {
+        super(message);
+    }
+}
