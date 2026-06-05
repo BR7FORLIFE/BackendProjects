17aa8adfc8227e44980c5b8dffd00606db6adf61
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-05 08:42:22 -0500

    feat: started project with Spring WebFlux

---
+++ LiveProductLive/src/main/java/com/files/LiveProductLive/LiveProductLiveApplication.java
@@ -0,0 +1,13 @@
+package com.files.LiveProductLive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveProductLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveProductLiveApplication.class, args);
+	}
+
+}
---
+++ LiveProductLive/src/test/java/com/files/LiveProductLive/LiveProductLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.files.LiveProductLive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveProductLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
