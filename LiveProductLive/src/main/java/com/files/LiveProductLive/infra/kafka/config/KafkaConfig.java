379df4a13591ef14120de5b8b4ee47f94e6464a3
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-07 12:05:51 -0500

    feat: add Kafka configuration and implement getAllProducts method in ProductController

---
+++ LiveProductLive/src/main/java/com/files/LiveProductLive/infra/kafka/config/KafkaConfig.java
@@ -0,0 +1,8 @@
+package com.files.LiveProductLive.infra.kafka.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+    
+}
--- LiveProductLive/src/main/java/com/files/LiveProductLive/infra/products/controller/ProductController.java
+++ LiveProductLive/src/main/java/com/files/LiveProductLive/infra/products/controller/ProductController.java
@@ -4,13 +4,14 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.files.LiveProductLive.application.products.dtos.CreateProductRequestDto;
+import com.files.LiveProductLive.application.products.dtos.GetAllProductsResponseDto;
 import com.files.LiveProductLive.application.products.usecases.ProductUseCase;
 
 import reactor.core.publisher.Mono;
@@ -31,9 +32,16 @@ public class ProductController {
                 .map(result -> ResponseEntity.ok().body(result));
     }
 
-    @GetMapping
-    public Mono<ResponseEntity<Void>> viewProduct(@RequestParam UUID id) {
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Void>> viewProduct(@PathVariable UUID id) {
         return productUseCase.establishViewByProductId(id)
                 .thenReturn(ResponseEntity.ok().build());
+    }
+
+    @GetMapping
+    public Mono<ResponseEntity<GetAllProductsResponseDto>> getAllProducts() {
+        return productUseCase.getAll()
+                .map(GetAllProductsResponseDto::new)
+                .map(ResponseEntity::ok);
     }
 }
