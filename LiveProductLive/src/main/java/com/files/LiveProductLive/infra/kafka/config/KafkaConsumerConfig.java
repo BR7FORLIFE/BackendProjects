3f660c66c6830fee54bca3ec5cbdf3754cb55272
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-08 15:11:51 -0500

    feat: add KafkaConsumerConfig and KafkaProducerConfig classes for Kafka integration

---
+++ LiveProductLive/src/main/java/com/files/LiveProductLive/infra/kafka/config/KafkaConsumerConfig.java
@@ -0,0 +1,45 @@
+package com.files.LiveProductLive.infra.kafka.config;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.files.LiveProductLive.infra.kafka.records.KafkaProperties;
+
+import reactor.kafka.receiver.ReceiverOptions;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    private final KafkaProperties properties;
+
+    public KafkaConsumerConfig(KafkaProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public ReceiverOptions<String, String> receiverOptions() {
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.bootstrapServer());
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, properties.consumerGroup()); // consumer group
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        // sino tengo offset guardados (mensajes) empieza desde el principio como el
+        // from-beginning
+        // en caso tal que si haya offset el parametro earliest se ignorará y se
+        // readudara desde el ultimo offset
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        ReceiverOptions<String, String> options = ReceiverOptions.create(config);
+
+        // nos subcribimos al topic como consumidores
+        return options.subscription(List.of(properties.productViewTopic()));
+    }
+}
---
+++ LiveProductLive/src/main/java/com/files/LiveProductLive/infra/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,36 @@
+package com.files.LiveProductLive.infra.kafka.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.files.LiveProductLive.infra.kafka.records.KafkaProperties;
+
+import reactor.kafka.sender.SenderOptions;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    private final KafkaProperties properties;
+
+    public KafkaProducerConfig(KafkaProperties kafkaProperties) {
+        this.properties = kafkaProperties;
+    }
+
+    // serializar es de objeto -> byte
+    // deserializar es de byte -> objeto
+    @Bean
+    public SenderOptions<String, String> senderOptions() {
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.bootstrapServer());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return SenderOptions.create(config);
+    }
+}
