8a70deef9a6fa5dea9efe037f1b5b1b33797037a
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-07-15 19:59:00 -0500

    feat: add initial application configuration, TestController, and test class

---
+++ Agents-Scheduling-MCP/src/test/java/com/files/Agents_Scheduling_MCP/AgentsSchedulingMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.files.Agents_Scheduling_MCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentsSchedulingMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
